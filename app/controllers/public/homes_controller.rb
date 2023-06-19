e7fa2b03d4e9dbb16b4df56dae838763615228c4
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-06-19 14:22:01 +0000

    Add a list of posts to the top page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,18 @@
 class Public::HomesController < ApplicationController
   
   def top
+    @reviews = Review.all
+    @reviews_latest5 = @reviews.first(5)
   end
   
   def about
+  end
+  
+  
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :beer_image, :brewery)
   end
   
 end
