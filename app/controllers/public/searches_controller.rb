bd4c5c6458b03ff51812b38434ece6d49d327c91
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-06-19 11:02:01 +0000

    Add

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,16 +1,16 @@
 class Public::SearchesController < ApplicationController
   
-　# #def search
-	# 	@model = params[:model]
-	# 	@content = params[:content]
-	# 	@method = params[:method]
-	# 	if @model == 'user'
-	# 		@records = User.search_for(@content, @method)
-	# 	elsif @model == 'review'
-	# 		@records = Review.search_for(@content, @method)
-	# 	else
-	# 		@records = Tag.search_for(@content, @method)
-	# 	end
-　# end
+	def search
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		elsif @model == 'review'
+			@records = Review.search_for(@content, @method)
+		else
+			@records = Tag.search_for(@content, @method)
+		end
+	end
   
 end
