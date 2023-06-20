608cbaf0f0a4afd01ff2631f98a632d72981035c
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-06-20 12:50:02 +0000

    [Fix] Migration

--- db/migrate/20230607001635_create_reviews.rb
+++ db/migrate/20230607001635_create_reviews.rb
@@ -1,13 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.string :name,                 null: false, default: ""
-      t.string :brewery,              null: false, default: ""
-      t.text :location,               null: false, default: ""
+      t.string :name,                 null: false
+      t.string :brewery,              null: false
+      t.text :location,               null: false
       t.integer :hoppy
       t.integer :acidity
       t.integer :sweetness
-      t.float :evaluation,            null: false, default: ""
+      t.float :evaluation,            null: false
       t.text :comment
       t.datetime :remember_created_at
       t.timestamps
--- db/migrate/20230608004816_create_review_tags.rb
+++ db/migrate/20230608004816_create_review_tags.rb
@@ -1,8 +1,8 @@
 class CreateReviewTags < ActiveRecord::Migration[6.1]
   def change
     create_table :review_tags do |t|
-      t.integer :review_id, null: false, default: ""
-      t.integer :tag_id, null: false, default: ""
+      t.integer :review_id, null: false
+      t.integer :tag_id, null: false
       t.timestamps
     end
   end
